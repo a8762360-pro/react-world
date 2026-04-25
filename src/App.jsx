9f9dab6748f22a4344a14acfe7c58eff3787e9f6
md alomgir <a8762360-pro>
2026-04-25 17:04:19 +0600

    react-country

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Countrys from './components/countrys/countrys'
+
+function App() {
+
+    return (
+    <>
+    <Countrys></Countrys>
+    </>
+) 
+}
+
+export default App
---
+++ src/components/countrys/countrys.jsx
@@ -0,0 +1,8 @@
+// import React from "react";
+export default function Countrys () {
+    return (
+        <div>
+            <h1>In the Country</h1>
+        </div>
+    )
+}
